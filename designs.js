26d431e279485a12a56573349d94aab0b88fe713
Julius Browning <jb068c@att.com>
2018-03-22 23:19:08 -0500

    javascript function for canvas

--- designs.js
+++ designs.js
@@ -2,9 +2,22 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+const myTable = document.getElementById(`pixelCanvas`);
 function makeGrid() {
+  console.log("connected");
 
-// Your code goes here!
+    const row = document.querySelector(`#inputHeight`).value;
+    const col = document.querySelector(`#inputWeight`).value;
+    for (let i = 1; i <= row; i++){
+      const newRow = document.createElement("tr")
+      for (let y = 1; i <= col; y++){
+        const newCol = document.createElement("td");
+        newRow[i].appendChild(newCol);
+        }
+        myTable.appendChild(newRow)
+    }
+    document.body.appendChild(myTable);
+  };
 
-}
+const btn = document.getElementsByTagName(`input`);
+  btn[2].addEventListener("click", makeGrid);
