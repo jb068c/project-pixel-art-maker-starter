09ba68b2590dfb4ee6c7ef7e50649c88d14f22fd
Julius Browning <jb068c@att.com>
2018-03-23 06:56:01 -0500

    corrected for loop

--- designs.js
+++ designs.js
@@ -6,18 +6,18 @@ let myTable = document.getElementById(`pixelCanvas`);
 function makeGrid() {
   console.log("connected");
 
-    const row = document.querySelector(`#inputHeight`).value;
-    const col = document.querySelector(`#inputWeight`).value;
+  const col = document.querySelector(`#inputWeight`).value;
+  const row = document.querySelector(`#inputHeight`).value;
     for (let i = 1; i <= row; i++){
       const newRow = document.createElement("tr")
-      for (let y = 1; i <= col; y++){
+      for (let y = 1; y <= col; y++){
         const newCol = document.createElement("td");
-        newRow[i].appendChild(newCol);
+        newRow.appendChild(newCol);
         }
         myTable.appendChild(newRow)
     }
-  };
     document.body.appendChild(myTable);
+  };
 
 const btn = document.getElementsByTagName(`input`);
   btn[2].addEventListener("click", makeGrid, false);
