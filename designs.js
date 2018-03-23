e35358ded1568d6f4002180b88fc138e353a88f2
Julius Browning <jb068c@att.com>
2018-03-23 17:42:31 -0500

    color picker

--- designs.js
+++ designs.js
@@ -3,7 +3,7 @@
 
 // When size is submitted by the user, call makeGrid()
 let myTable = document.getElementById(`pixelCanvas`);
-function makeGrid() {
+function makeGrid(evt) {
   console.log("connected");
 
   const col = document.querySelector(`#inputWeight`).value;
@@ -11,13 +11,21 @@ function makeGrid() {
     for (let i = 1; i <= row; i++){
       const newRow = document.createElement("tr")
       for (let y = 1; y <= col; y++){
-        const newCol = document.createElement("td");
+        const newCol = document.createElement("td" + evt.target.style.color);
         newRow.appendChild(newCol);
         }
         myTable.appendChild(newRow)
     }
     document.body.appendChild(myTable);
+    console.log(evt);
+    evt.preventDefault();
+
   };
 
 const btn = document.getElementsByTagName(`input`);
-  btn[2].addEventListener("click", makeGrid, false);
+  btn[2].addEventListener("click", makeGrid);
+
+const cell = document.getElementsByTagName('td');
+myTable.addEventListener("click", function(evt){
+  evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
+});
